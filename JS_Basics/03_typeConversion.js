752bc495fd1b0619965bb814c8147729364e5f88
Swapnil <44332884+swapnildwivedi21@users.noreply.github.com>
2024-06-04 19:26:55 +0000

    added code for type conversion

--- JS_Basics/03_typeConversion.js
+++ JS_Basics/03_typeConversion.js
@@ -4,3 +4,16 @@ let valueInNumber = Number(score)
 console.log(typeof(score))
 console.log(typeof(valueInNumber))
 console.log(typeof String(valueInNumber))
+
+let score1  = "33abc"
+console.log(typeof(score1))
+// if there is a number combined with the string , the printing post conversion will give "Nan"
+console.log(Number(score1))
+
+let score2 = null
+console.log(score2)
+
+let score3 =""
+let score3boolean = Boolean(score3)
+console.log(score3boolean)
+
